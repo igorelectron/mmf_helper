55feb42e82e6a1929b33b019ba1308ca2747f7c5
Nedilia Ihor <igor.nedelya@gmail.com>
2024-07-05 11:50:02 +0300

    added reading rings schedule and saving it to the file, also managing them + schedule save manager

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ from os import getenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message, KeyboardButton
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
@@ -16,6 +16,74 @@ TOKEN = getenv("BOT_TOKEN")
 
 dp = Dispatcher()
 
+class Ring:
+    
+    def __init__(self, start_time: str, end_time: str):
+        
+        self.start_time = start_time
+        self.end_time = end_time
+    
+    def __str__(self) -> str:
+        return f"{self.start_time}-{self.end_time}"
+
+class Schedule_Save_Manager:
+    def __init__(self):
+        pass
+    
+    def __enter__(self):
+        try:
+            with open("rings.txt") as f:
+                for line in f.readlines():
+                    key, value = line.split()
+                    all_rings.all_rings[key] = value
+        except FileNotFoundError:
+            pass
+        
+    def __exit__(self, _a, _b, _c):
+            with open("rings.txt", 'w') as f:
+                file_string = ""
+                for key, value in all_rings.all_rings.items():
+                    file_string += f"{key} {value}\n"
+                f.write(file_string)
+            
+class Rings_Schedule:
+    
+    def __init__(self):
+        self.all_rings = {}
+        
+all_rings = Rings_Schedule()
+    
+@dp.message(Command("show_rings"))    
+async def show_all_rings(message: Message):
+    answer_string = "Всі дзвінки:\n"
+    for key, value in all_rings.all_rings.items():
+        answer_string += f"{key} - {value}\n"
+    await message.answer(answer_string)
+        
+@dp.message(Command("add_rings"))    
+async def add_rings(message: Message):
+    parts = message.text.split()
+    if len(parts) == 3:
+        key = parts[1]
+        value = parts[2]
+        all_rings.all_rings[key] = value
+        await message.answer("OK")
+    else:
+        await message.answer("Тут щось не те")
+        
+        
+@dp.message(Command("delete_rings"))
+async def delete_ring(message: Message):
+    parts = message.text.split()
+    if len(parts) == 2:
+        key = parts[1]
+        if key not in all_rings.all_rings:
+            await message.answer("Такої пари нема")
+        else:
+            del(all_rings.all_rings[key])
+            await message.answer("OK")
+    else:
+        await message.answer("Тут щось не те")
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -51,9 +119,11 @@ async def main() -> None:
 
     # And the run events dispatching
     await dp.start_polling(bot)
+    
+
 
 
 if __name__ == "__main__":
-    print('smth')
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    with Schedule_Save_Manager():
+        asyncio.run(main())
